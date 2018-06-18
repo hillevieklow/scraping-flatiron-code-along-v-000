ad51a364820ab20be8cfdc9477961eee44a1ffde
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-18 19:39:05 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,5 +1,5 @@
 class Course
-  attr_accessors :title, :schedule, :description
+  attr_accessor :title, :schedule, :description
 
   @@all = []
 
@@ -14,4 +14,5 @@ class Course
   def self.reset_all
     @@all.clear
   end
+
 end
